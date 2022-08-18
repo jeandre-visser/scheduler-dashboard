f4e32fe2b3392f60b2bdf5a41d259c4f36857900
Jeandre <jeandrev1414@gmail.com>
2022-08-18 15:41:00 -0600

    Implement an instance to focus on panel and toggle back to all panels

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Loading from "./Loading";
 import classnames from "classnames";
 import Panel from "./Panel";
+import { identical } from "lodash/fp";
 
 const data = [
   {
@@ -29,9 +30,14 @@ const data = [
 class Dashboard extends Component {
   state = { loading: false, focused: null };
 
+  selectPanel(id) {
+    this.setState(prevState => ({
+      focused: prevState.focused !== null ? null : id
+    }));
+  }
   render() {
     const dashboardClasses = classnames("dashboard", {
-      "dashboard--focused": this.state.focused
+      "dashboard--focused": this.state.focused,
     });
 
     if (this.state.loading) {
@@ -42,16 +48,17 @@ class Dashboard extends Component {
       this.state.focused
         ? data.filter((panel) => this.state.focused === panel.id)
         : data
-    ).map((panel) => {
+    ).map((panel) => (
       <Panel
         key={panel.id}
         id={panel.id}
         label={panel.label}
         value={panel.value}
-      />;
-    });
+        onSelect={event => this.selectPanel(panel.id)}
+      />
+    ));
 
-    return <main className={dashboardClasses} />;
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
